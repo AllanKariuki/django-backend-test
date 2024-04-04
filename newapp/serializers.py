4688ac24566d10d6d7db07b786461b4c62eeaacc
AllanKariuki <alankariuki24@gmail.com>
2024-04-04 14:34:19 +0300

    fix: deletion of newapp

--- newapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import NewApp
-
-class NewAppSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = NewApp
-        fields = '__all__'
