a9aa7ecc0bf51d9f0f60ca5c523f36c1ffea396d
AllanKariuki <alankariuki24@gmail.com>
2024-04-05 11:56:21 +0300

    addition of corsheaders

--- test_submission_django/settings.py
+++ test_submission_django/settings.py
@@ -31,9 +31,11 @@ INSTALLED_APPS = [
     'users',
     'rest_framework',
     'accounts',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +141,25 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:8000"
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'access-control-allow-origin',
+    'accept-encoding',
+    'authorization',  # Include the 'authorization' header
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
